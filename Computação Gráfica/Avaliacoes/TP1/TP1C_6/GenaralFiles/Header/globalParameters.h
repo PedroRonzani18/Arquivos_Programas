d4745d380a10f46506da53ba7273943b65c2f3bc
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2022-10-14 22:02:54 -0300

    14/10

--- Computação Gráfica/Avaliacoes/TP1/TP1C_6/GenaralFiles/Header/globalParameters.h
+++ Computação Gráfica/Avaliacoes/TP1/TP1C_6/GenaralFiles/Header/globalParameters.h
@@ -7,9 +7,10 @@ extern double aspectRatio;
 extern int keys[6];
 extern GLuint aviaoDisplayList, aviaoDisplayList1, aviaoDisplayList2, shotDisplayList;
 
+/*
 template<typename Base, typename T>
 extern inline bool instanceof(const T *ptr) {
    return dynamic_cast<const Base*>(ptr) != nullptr;
-}
+}*/
 
 #endif
--- Computação Gráfica/Avaliacoes/TP1/TP1C_7/GenaralFiles/Header/globalParameters.h
+++ Computação Gráfica/Avaliacoes/TP1/TP1C_7/GenaralFiles/Header/globalParameters.h
@@ -5,7 +5,11 @@
 
 extern double aspectRatio;
 extern int keys[6];
-extern GLuint aviaoDisplayList, aviaoDisplayList1, aviaoDisplayList2, shotDisplayList;
+extern GLuint aviaoDisplayList, aviaoDisplayList1, aviaoDisplayList2, shotDisplayList, buttonDisplayList;
+extern int sposition;
+extern int flag;
+extern int tempo;
+
 
 template<typename Base, typename T>
 extern inline bool instanceof(const T *ptr) {
